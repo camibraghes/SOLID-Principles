3bebada273dc95fc4bf9cc7824bbbe85ffce32ec
Camelia <cameliabraghes@gmail.com>
2023-02-01 09:41:55 +0200

    First commit

---
+++ SOLID Principles/SOLID_PrinciplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOLID_PrinciplesApp.swift
+//  SOLID Principles
+//
+//  Created by Camelia Braghes on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SOLID_PrinciplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
